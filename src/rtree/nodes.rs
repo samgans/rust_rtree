d14601dcf071a23d0fa3e5b50e7016f686eec8a5
samgans <antonzhiltsov2000@gmail.com>
2021-06-15 09:59:20 +0300

    Add Split feature template to RTree

--- src/rtree/nodes.rs
+++ src/rtree/nodes.rs
@@ -140,3 +140,241 @@ impl RtreeObject for RtreeNode {
         self.parent = Some(Rc::downgrade(node));
     }
 }
+
+impl RtreeSplit for RtreeNode {
+
+    fn split(&mut self) -> () {
+        let nodes = self.execute();
+        self.children = nodes.0.children;
+        self.mbr = nodes.0.mbr;
+
+        match self.children {
+            ChildrenType::InnerNodes(ref mut nodes) => {
+                let self_ptr = self as *const RtreeNode;
+                let self_rc = Weak::from_raw(self_ptr).upgrade();
+
+                if let Some(ref ptr) = self_rc {
+                    for node in nodes {
+                        node.set_parent(ptr)
+                    }
+                }
+            }
+        }
+    }
+
+    fn execute(&mut self) -> (Rc<RtreeNode>, Rc<RtreeNode>) {
+        let node_1: Rc<RtreeNode>;
+        let node_2: Rc<RtreeNode>;
+        match self.children {
+            ChildrenType::InnerNodes(ref mut nodes) => {
+                let picked = self.pick_seeds(nodes, ChildrenType::InnerNodes);
+                node_1 = picked.0;
+                node_2 = picked.1;
+                self.distribute_nodes(&mut node_1, &mut node_2, nodes);
+            },
+            ChildrenType::Leafs(ref mut leafs) => {
+                let picked = self.pick_seeds(leafs, ChildrenType::Leafs);
+                node_1 = picked.0;
+                node_2 = picked.1;
+                self.distribute_leafs(&mut node_1, &mut node_2, leafs);
+            }
+        };
+        (node_1, node_2)
+    }
+
+    fn pick_seeds<T: RtreeObject>(&mut self, objects: &mut Vec<Rc<T>>, 
+                                  child_type: fn(Vec<Rc<T>>) -> ChildrenType)
+                -> (Rc<RtreeNode>, Rc<RtreeNode>) {
+        let mut max_area = NEGINF;
+        let mut objs = MaybeUninit::<(usize, usize)>::uninit();
+        
+        for i in 0..objects.len() {
+            let obj_1 = &objects[i];
+            for j in i + 1..objects.len() {
+                let obj_2 = &objects[j];
+                let common_mbr = BoundingRectangle::common_mbr(
+                    &vec!(&obj_1.mbr(), &obj_2.mbr())
+                );
+                if common_mbr.area > max_area {
+                    max_area = common_mbr.area;
+                    unsafe {
+                        objs.as_mut_ptr().write((i, j))
+                    }
+                };
+            }
+        };
+
+        let objs = unsafe {
+            objs.assume_init()
+        };
+
+        let mut obj_1 = objects.remove(objs.0);
+        let mut obj_2 = objects.remove(objs.1 - 1);
+
+        let mut node_1 = RtreeNode::new(
+            *obj_1.mbr(),
+            self.max_children,
+            child_type
+        );
+        let mut node_2 = RtreeNode::new(
+            *obj_2.mbr(),
+            self.max_children,
+            child_type
+        );
+
+        node_1.children = child_type(vec!(obj_1));
+        node_2.children = child_type(vec!(obj_2));
+
+        (Rc::new(node_1), Rc::new(node_2))
+    }
+
+    fn distribute_nodes(
+        &self,
+        node_1: &mut Rc<RtreeNode>,
+        node_2: &mut Rc<RtreeNode>,
+        objects: &mut Vec<Rc<RtreeNode>>
+    ) -> () {
+        for i in 0..objects.len() {
+            if self.validate_nodes_quantity(node_1, node_2, objects) {
+                break
+            } else {
+                let insert_obj = self.pick_next_node(node_1, node_2, objects);
+                let to_insert = find_least_enlargement(
+                    &mut vec!(node_1, node_2),
+                    &insert_obj.mbr
+                );
+                to_insert.0.mbr = to_insert.1;
+                to_insert.0.children.add_node(insert_obj);
+            }
+        }
+    }
+
+    fn distribute_leafs(
+        &self,
+        node_1: &mut Rc<RtreeNode>,
+        node_2: &mut Rc<RtreeNode>,
+        objects: &mut Vec<Rc<RtreeGeometry>>
+    ) -> () {
+        for i in 0..objects.len() {
+            if self.validate_leafs_quantity(node_1, node_2, objects) {
+                break
+            } else {
+                let insert_obj = self.pick_next_leaf(node_1, node_2, objects);
+                let to_insert = find_least_enlargement(
+                    &mut vec!(node_1, node_2),
+                    &insert_obj.mbr
+                );
+                to_insert.0.mbr = to_insert.1;
+                to_insert.0.children.add_leaf(insert_obj);
+            }
+        }
+    }
+
+    fn pick_next_node(&self, node_1: &mut Rc<RtreeNode>, node_2: &mut Rc<RtreeNode>,
+                          objects: &mut Vec<Rc<RtreeNode>>)
+            -> Rc<RtreeNode> {
+        let min_d = INF;
+        let chosen: usize;
+
+        for i in 0..objects.len() {
+            let obj = objects[i];
+            let area_1 = BoundingRectangle::common_mbr(
+                &vec!(&node_1.mbr, &obj.mbr)
+            ).area;
+            let area_2 = BoundingRectangle::common_mbr(
+                &vec!(&node_2.mbr, &obj.mbr)
+            ).area;
+
+            let d = area_1 - area_2;
+            if d < min_d {
+                min_d = d;
+                chosen = i;
+            };
+        };
+        objects.remove(chosen)
+    }
+
+    fn pick_next_leaf(&self, node_1: &mut Rc<RtreeNode>, node_2: &mut Rc<RtreeNode>,
+                      objects: &mut Vec<Rc<RtreeGeometry>>) -> Rc<RtreeGeometry> {
+        let min_d = INF;
+        let chosen: usize;
+
+        for i in 0..objects.len() {
+            let obj = objects[i];
+            let area_1 = BoundingRectangle::common_mbr(
+                &vec!(&node_1.mbr, &obj.mbr)
+            ).area;
+            let area_2 = BoundingRectangle::common_mbr(
+                &vec!(&node_2.mbr, &obj.mbr)
+            ).area;
+
+            let d = area_1 - area_2;
+            if d < min_d {
+                min_d = d;
+                chosen = i;
+            };
+        };
+        objects.remove(chosen)
+    }
+
+    fn pick_underfull<'a>(&'a self, node_1: &'a mut Rc<RtreeNode>, node_2: &'a mut Rc<RtreeNode>)
+            -> Option<&'a mut Rc<RtreeNode>>  {
+        let to_final_push: Option<&mut Rc<RtreeNode>> = None;
+        let max_objs = node_1.max_children;
+        let peak = (max_objs - max_objs / 2) + 1;
+
+        if node_1.children.len() == peak as usize {
+            to_final_push = Some(node_1);
+        } else if node_2.children.len() == peak as usize {
+            to_final_push = Some(node_2);
+        };
+
+        to_final_push
+    }
+
+    fn validate_nodes_quantity(
+        &self,
+        node_1: &mut Rc<RtreeNode>,
+        node_2: &mut Rc<RtreeNode>,
+        objects: &mut Vec<Rc<RtreeNode>>
+    ) -> bool {
+
+        let to_final_push = self.pick_underfull(node_1, node_2);
+        match to_final_push {
+            Some(value) => {
+                for i in 0..objects.len() {
+                    let obj = objects.remove(i);
+                    let new_mbr = BoundingRectangle::common_mbr(
+                        &vec!(obj.mbr(), &value.mbr)
+                    );
+                    value.children.add_node(obj) 
+                }
+                true
+            },
+            None => false
+        }
+    }
+
+    fn validate_leafs_quantity(
+        &self,
+        node_1: &mut Rc<RtreeNode>,
+        node_2: &mut Rc<RtreeNode>,
+        objects: &mut Vec<Rc<RtreeGeometry>>
+    ) -> bool {
+
+        let to_final_push = self.pick_underfull(node_1, node_2);
+        match to_final_push {
+            Some(value) => {
+                for i in 0..objects.len() {
+                    let obj = objects.remove(i);
+                    let new_mbr = BoundingRectangle::common_mbr(
+                        &vec!(obj.mbr(), &value.mbr)
+                    );
+                    value.children.add_leaf(obj) 
+                }
+                true
+            },
+            None => false
+        }
+    }
+}
